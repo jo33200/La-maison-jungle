6d5c3467efcebc7c2cffd71c30f74ca26e16a3ee
jo33200 <jo33200@gmail.com>
2024-03-29 14:53:05 +0100

    build: banner

---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+
+function Banner() {
+    return (
+        <div className="lmj-banner">
+            <img src={logo} alt='La maison jungle' className='lmj-logo' />
+            <h1>La maison jungle</h1>
+        </div>
+    )
+}
+
+<div className="lmj-banner">
+    <h1>La maison jungle</h1>
+</div>
+
+export default Banner
