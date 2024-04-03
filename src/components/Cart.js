ed5fe1eb0dd393db3b3369e0a4325468195eb1ba
jo33200 <jo33200@gmail.com>
2024-04-03 16:26:51 +0200

    build: useState

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,19 @@
 import '../styles/cart.css'
+import { useState } from 'react'
 
 function Cart() {
 	const monsteraPrice = 8
-	const ivyPrice = 10
-	const flowerPrice = 15
+	const [cart, updateCart] = useState(0)
 	return (
 		<div className='lmj-cart'>
 			<h2>Panier</h2>
-			<ul>
+			<div>
 				<li>Monstera : {monsteraPrice}€</li>
-				<li>Lierre : {ivyPrice}€</li>
-				<li>Fleurs : {flowerPrice}€</li>
-			</ul>
-			Total : {monsteraPrice + ivyPrice + flowerPrice}€
+				<button onClick={() => updateCart(cart + 1)}>
+					Ajouter
+				</button>
+			</div>
+			<h3>Total : {monsteraPrice*cart}€</h3>
 		</div>
 	)
 }
