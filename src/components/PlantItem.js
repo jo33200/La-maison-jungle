b713474cbc56de6fa7bf39f7e9be879d28dc69a5
jo33200 <jo33200@gmail.com>
2024-04-03 15:06:37 +0200

    build: evenement click

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -4,7 +4,7 @@ import '../styles/plantItem.css'
 
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item'>
+		<li key={id} className='lmj-plant-item' onClick={()=>{handleClick(name)}}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
@@ -13,6 +13,10 @@ function PlantItem({ id, cover, name, water, light }) {
 			</div>
 		</li>
 	)
+}
+
+function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix🌱✨`)
 }
 
 export default PlantItem
