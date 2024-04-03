798879cd7ec60a35eaeee35c9cb7e7fc7304c44f
jo33200 <jo33200@gmail.com>
2024-04-03 16:34:49 +0200

    build: button panier

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,20 +2,25 @@ import '../styles/cart.css'
 import { useState } from 'react'
 
 function Cart() {
-	const monsteraPrice = 8
-	const [cart, updateCart] = useState(0)
-	return (
-		<div className='lmj-cart'>
-			<h2>Panier</h2>
-			<div>
-				<li>Monstera : {monsteraPrice}€</li>
-				<button onClick={() => updateCart(cart + 1)}>
-					Ajouter
-				</button>
-			</div>
-			<h3>Total : {monsteraPrice*cart}€</h3>
-		</div>
-	)
+    const monsteraPrice = 8
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(false)
+ 
+    return isOpen ? (
+        <div className='lmj-cart'>
+            <button onClick={() => setIsOpen(false)}>Fermer</button>
+            <h2>Panier</h2>
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>
+                    Ajouter
+                </button>
+            </div>
+            <h3>Total : {monsteraPrice * cart}€</h3>
+        </div>
+    ) : (
+        <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
+    )
 }
 
 export default Cart
